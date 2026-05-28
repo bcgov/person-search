a2b4d9203a1cbc3dad78211978cc087049b9fc0b
bolyachevets <andriy.bolyachevets@gov.bc.ca>
2026-05-28 12:54:41 -0700

    unit tests

--- bor-api/src/bor_api/config.py
+++ bor-api/src/bor_api/config.py
@@ -163,7 +163,7 @@ class UnitTestingConfig(Config):
     DB_NAME = os.getenv("DATABASE_TEST_NAME", "")
     DB_HOST = os.getenv("DATABASE_TEST_HOST", "")
     DB_PORT = os.getenv("DATABASE_TEST_PORT", "5432")
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{int(DB_PORT)}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{int(DB_PORT)}/{DB_NAME}"
 
     # JWT OIDC settings
     # JWT_OIDC_TEST_MODE will set jwt_manager to use
